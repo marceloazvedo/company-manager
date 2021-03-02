8cf1482c2114ea74070a8bd30fc6592e7114349e
Marcelo Azevedo <marceloazvedo1@gmail.com>
2021-03-01 22:06:44 -0300

    Initial project configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.postgresql:postgresql:42.2.1")
 	runtimeOnly("io.micrometer:micrometer-registry-new-relic")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
